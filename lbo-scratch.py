97c592233c2689e021fa880487ca1b78414e517b
Leon van Bokhorst <leon@remondo.net>
2015-02-11 22:44:04 +0100

    Ex.1 Cookie problem

--- lbo-scratch.py
+++ lbo-scratch.py
@@ -6,7 +6,7 @@ pmf.Set('Bowl 1', .5)               # P(Bowl 1)
 pmf.Set('Bowl 2', .5)               # P(Bowl 2)
 pmf.Print()
 
-# picks vanilla cookie - what is P of bowl
+# picks vanilla cookie - update P of bowl with evidence
 pmf.Mult('Bowl 1', .75)             # P(Vanilla | Bowl 1)
 pmf.Mult('Bowl 2', .5)              # P(Vanilla | Bowl 2)
 pmf.Print()
@@ -14,7 +14,7 @@ pmf.Print()
 print pmf.Normalize()
 pmf.Print()
 
-# picks chocolate cookie - update the pmf
+# picks chocolate cookie - update the pmf with new evidence
 pmf.Mult('Bowl 1', .25)             # P(Chocolate | Bowl 1)
 pmf.Mult('Bowl 2', .5)              # P(Chocolate | Bowl 2)
 pmf.Print()
